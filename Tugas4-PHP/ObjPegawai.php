f6aa7d4f4d8a9b00146724121f3dcdbfc7d1fb9c
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2022-10-02 13:13:32 +0700

    Update Tugas 4 PHP

--- Tugas4-PHP/ObjPegawai.php
+++ Tugas4-PHP/ObjPegawai.php
@@ -1,16 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tugas 4 PHP | Asep Sahrudin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="text-center py-4">Daftar Pegawai</h1>
+    <!-- first table -->
+  <table>
+  <tbody>
 <?php 
 require 'Pegawai.php';
 
 // ciptakan object
-$asepsahrudin = new Pegawai('242424', 'Asep Sahrudin', 'Manager', 'Islam', 'Menikah');
-$alfian = new Pegawai('242424', 'Alfian', 'Asmen', 'Islam', 'Lajang');
-$andreas = new Pegawai('242424', 'Andreas', 'Kabag', 'Kristen', 'Menikah');
-$superman = new Pegawai('242424', 'Superman', 'Kabag', 'Hindu', 'Lajang');
-$sandika = new Pegawai('242424', 'Sandika', 'Staff', 'Islam', 'Lajang');
-$asepsahrudin->mencetak();
-$alfian->mencetak();
-$andreas->mencetak();
-$superman->mencetak();
-$sandika->mencetak();
-echo '<br />Jumlah Pegawai: '.Pegawai::$total;
+$data1 = new Pegawai('242435', 'Asep Sahrudin', 'Manager', 'Islam', 'Menikah');
+$data2 = new Pegawai('242459', 'Alfian', 'Asmen', 'Islam', 'Lajang');
+$data3 = new Pegawai('242348', 'Andreas', 'Kabag', 'Kristen', 'Menikah');
+$data4 = new Pegawai('242449', 'Superman', 'Kabag', 'Hindu', 'Lajang');
+$data5 = new Pegawai('242430', 'Sandika', 'Staff', 'Islam', 'Lajang');
+
+$data_pegawai = [$data1, $data2, $data3, $data4, $data5];
+
+echo '<b><u>'.Pegawai::PEGAWAI.'</u></b>';
 ?>
+<?php
+foreach($data_pegawai as $dp){
+    ?>
+    
+      <tr><th><?= $dp->mencetak()?></th></tr>
+    <?php
+}
+echo '<br /><p class="mb-3">Jumlah Pegawai: '.Pegawai::$total.'</p>';
+?>
+</tbody>
+ <!-- last table -->
+    </table>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
--- Tugas4-PHP/Pegawai.php
+++ Tugas4-PHP/Pegawai.php
@@ -1,4 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tugas 4 PHP | Asep Sahrudin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body >
+    
+    <!-- first table -->
+    <div class="table-responsive">
+    <table class="table table-hover table-bordered text-center">
+    <!-- header table -->
+    <thead>
+      <tr bgcolor="#937DC2" class="text-light">
+        <?php
+        $table_title = ['NIP', 'NAMA', 'JABATAN', 'AGAMA', 'STATUS', 'GAJI POKOK', 'TUNJANGAN JABATAN', 'TUNJANGAN KELUARGA' , 'ZAKAT' , 'GAJI KOTOR', 'TAKE HOME PAY']; 
+        foreach($table_title as $tt){
+            ?>
+        <th><?= $tt?></th>
+       <?php }
+        ?>
+      </tr>
+    </thead>
+    <!-- last header -->
+    <tbody>
+
+    <!-- my php script -->
 <?php 
+    // class pegawai 
     class Pegawai{
         public $nip;
         public $nama;
@@ -20,6 +50,7 @@
             self::$total++;
         }
 
+        // fungsi perhitungan gaji pokok
         public function setGapok(){
             switch ($this->jabatan) {
                 case 'Manager': $gapok = 15000000; break;
@@ -31,18 +62,22 @@
             return $gapok;     
         }
 
+        // fungsi tunjangan jabatan
         public function setTunjab(){
             return $tunjab = $this->setGapok() * 0.2;
         }
 
+        // fungsi tunjangan keluarga
         public function setTunkel(){
             return $tunkel = $this->status == 'Menikah' ? $this->setGapok() * 0.1 : 0; 
         }
 
+        // fungsi gaji kotor
         public function brutto(){
             return $this->brutto = $this->setGapok() + $this->setTunjab() + $this->setTunkel();
         }
 
+        // fungsi zakat profesi
         public function setZakatProfesi(){
             $zakat = 0;
             if ($this->agama == 'Islam' && $this->brutto() >= 6000000) {
@@ -53,26 +88,47 @@
             return $zakat;
         }
 
+        // fungsi take home pay / gaji bersih yang dibawa kerumah
         public function setThPay(){
             return  $tHPay = $this->setGapok() - $this->setZakatProfesi();
         }
 
-
+        // fungsi cetak
         public function mencetak(){
-            echo '<b><u>'.self::PEGAWAI.'</u></b>';
-            echo '<br />';
-            echo '<br />NIP: '.$this->nip;
-            echo '<br />Nama: '.$this->nama;
-            echo '<br />Jabatan: '.$this->jabatan;
-            echo '<br />Agama: '.$this->agama;
-            echo '<br />Status: '.$this->status;
-            echo '<br />Gaji Pokok: '.$this->setGapok();
-            echo '<br />Tunjangan Jabatan: '.$this->setTunjab();
-            echo '<br />Tunjangan Keluarga: '.$this->setTunkel();
-            echo '<br />Zakat: '.$this->setZakatProfesi();
-            echo '<br />Gaji Kotor: '.$this->brutto();
-            echo '<br />Gaji Bersih: '.$this->setThPay();
-            echo '<hr />';
-        }
+            // data buat ditampilkan ke table
+            $table_title = [
+                'NIP'=>$this->nip, 
+                'NAMA'=>$this->nama, 
+                'JABATAN'=>$this->jabatan, 
+                'AGAMA'=>$this->agama, 
+                'STATUS'=>$this->status, 
+                'GAJI POKOK'=>'Rp. '.number_format($this->setGapok(), 2, ',', '. '), 
+                'TUNJANGAN JABATAN'=>'Rp. '.number_format($this->setTunjab(), 2, ',', '. '), 
+                'TUNJANGAN KELUARGA'=>'Rp. '.number_format($this->setTunkel(), 2, ',', '. '), 
+                'ZAKAT'=>'Rp. '.number_format($this->setZakatProfesi(), 2, ',', '. '), 
+                'GAJI KOTOR'=>'Rp. '.number_format($this->brutto(), 2, ',', '. '), 
+                'TAKE HOME PAY'=>'Rp. '.number_format($this->setThPay(), 2, ',', '. ')
+            ]; 
+
+            ?>
+            <tr>
+            <?php
+
+            foreach($table_title as $tbt => $this->hasil){
+                ?>
+                    <th><?= $this->hasil ?></th>
+                    
+                <?php
+            }
+           ?>
+        <?php }
     }
 ?>
+</tr>
+</tbody> 
+
+</div>
+<!-- last table -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
