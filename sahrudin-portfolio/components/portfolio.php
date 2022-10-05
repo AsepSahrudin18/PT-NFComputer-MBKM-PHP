22122c3f2dd5cb2f2bbc0d8cf6dd08553144b219
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2022-10-05 23:47:51 +0700

    Update Tugas 6 PortFolio

---
+++ sahrudin-portfolio/components/portfolio.php
@@ -0,0 +1,125 @@
+<!-- ======= Portfolio Section ======= -->
+<section id="work" class="portfolio-mf sect-pt4 route">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="title-box text-center">
+                    <h3 class="title-a">
+                        Portfolio
+                    </h3>
+                    <p class="subtitle-a">
+                        Ini merupakan hasil dari PortFolio yang pernah saya kerjakan
+                    </p>
+                    <div class="line-mf"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="work-box">
+                    <a href="assets/img/work-1.svg" data-gallery="portfolioGallery" class="portfolio-lightbox">
+                        <div class="work-img">
+                            <img src="assets/img/work-1.svg" alt="" class="img-fluid">
+                        </div>
+                    </a>
+                    <div class="work-content">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <h2 class="w-title">Movie-App</h2>
+                                <div class="w-more">
+                                    <span class="w-ctegory">Web Developer | ReactJS</span> / <span class="w-date">18
+                                        Maret
+                                        2022</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="w-like">
+                                    <a href="./portfolio-details/movie-app.php"> <span
+                                            class=" bi bi-plus-circle"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="work-box">
+                    <a href="assets/img/work-2.svg" data-gallery="portfolioGallery" class="portfolio-lightbox">
+                        <div class="work-img">
+                            <img src="assets/img/work-2.svg" alt="" class="img-fluid">
+                        </div>
+                    </a>
+                    <div class="work-content">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <h2 class="w-title">Movie-Wiki</h2>
+                                <div class="w-more">
+                                    <span class="w-ctegory">Web Dev | Kawah Edukasi</span> / <span class="w-date">2
+                                        Oktober 2022</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="w-like">
+                                    <a href="./portfolio-details/movie-wiki.php"> <span
+                                            class="bi bi-plus-circle"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="work-box">
+                    <a href="assets/img/work-3.svg" data-gallery="portfolioGallery" class="portfolio-lightbox">
+                        <div class="work-img">
+                            <img src="assets/img/work-3.svg" alt="" class="img-fluid">
+                        </div>
+                    </a>
+                    <div class="work-content">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <h2 class="w-title">Data Covid Dashboard</h2>
+                                <div class="w-more">
+                                    <span class="w-ctegory">Frontend Developer</span> / <span class="w-date">2 April
+                                        2022</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="w-like">
+                                    <a href="./portfolio-details/data-covid.php"> <span
+                                            class="bi bi-plus-circle"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="work-box">
+                    <a href="assets/img/work-4.svg" data-gallery="portfolioGallery" class="portfolio-lightbox">
+                        <div class="work-img">
+                            <img src="assets/img/work-4.svg" alt="" class="img-fluid">
+                        </div>
+                    </a>
+                    <div class="work-content">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <h2 class="w-title">Data Engineer | ETL</h2>
+                                <div class="w-more">
+                                    <span class="w-ctegory">Data Engineer KemKominfo</span> / <span class="w-date">Juni
+                                        2022</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="w-like">
+                                    <a href="./portfolio-details/data-engineer.php"> <span
+                                            class="bi bi-plus-circle"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- End Portfolio Section -->
---
+++ sahrudin-portfolio/components/tasks.php
@@ -0,0 +1,98 @@
+<!-- ======= Services Section ======= -->
+<section id="tasks" class="services-mf pt-5 route">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="title-box text-center">
+                    <h3 class="title-a">
+                        MY TASKS
+                    </h3>
+                    <p class="subtitle-a">
+                        Ini merupakan repository tugas MBKM di PT Nurul Fikri Cipta Inovasi | Kampus Merdeka
+                    </p>
+                    <div class="line-mf"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-briefcase"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Web Design</h2>
+                        <p class="s-description text-center">
+                            HTML5, CSS3 dan Bootstrap5
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-card-checklist"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Javascript</h2>
+                        <p class="s-description text-center">
+                            Kumpulan tugas materi Javascript
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-bar-chart"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">design ui/ux</h2>
+                        <p class="s-description text-center">
+                            Kumpulan tugas materi Design UI UX
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-binoculars"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">PHP PROGRAMMING</h2>
+                        <p class="s-description text-center">
+                            Tugas tentang bahasa pemrograman PHP
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-brightness-high"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">MYSQL DATABASE</h2>
+                        <p class="s-description text-center">
+                            Kumpulan tugas materi tentang Database
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="bi bi-calendar4-week"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">GIT/GITHUB</h2>
+                        <p class="s-description text-center">
+                            Tugas tentang GIT Version Control System
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- End Services Section -->
